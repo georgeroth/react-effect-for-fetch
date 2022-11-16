c89a8a4eaa6088b600cdc0b692db333ee92a0697
George Roth <g@georgeroth.eu>
2022-11-16 17:59:43 +0100

    Artworks and users done

--- src/sections/Art/index.js
+++ src/sections/Art/index.js
@@ -1,10 +1,48 @@
 import { useEffect, useState } from "react"
 
 function ArtsSection() {
+  const [artworks, setArtworks] = useState([])
+
+
+useEffect(() => {
+  fetch(`https://api.artic.edu/api/v1/artworks?limit=10`)
+   .then((res) => res.json())
+   .then((data) => setArtworks(data.data)); 
+}, [])
+
+console.log(artworks)
+
   return (
     <section>
       <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
+        <div className="scroll-container">
+          <ul className="art-list">
+        {artworks.map((artwork, index) => {
+          return (
+            <li key={index}>
+              <div className="frame">
+                <img
+                  src={`https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg`}
+                alt={artwork.title}/>
+              </div>
+              <h3>{artwork.title}</h3>
+              <p>Artist: {artwork.artist_title}</p>
+              <h4>Artistic Subjects:</h4>
+              <ul>
+              {artwork.subject_titles.length === 0 ? <li>No artistic subjects found</li>
+              :              
+              artwork.subject_titles.map((title, index) => {
+                return (
+                  <li key={index}>{title}</li>   
+                )
+              })
+              }
+              </ul>
+            </li>
+          )
+        })}
+        </ul>
+      </div>
     </section>
   )
 }
--- src/sections/Users/index.js
+++ src/sections/Users/index.js
@@ -1,10 +1,45 @@
 import { useEffect, useState } from "react"
 
 function UsersSection() {
+  const [usersList, setUsersList] = useState([])
+  let backgroundColor = ""
+
+  useEffect(() => {
+    fetch(`https://randomuser.me/api/?results=10`)
+      .then((res) => res.json())
+      .then((data) => setUsersList(data.results))
+  }, [])
+
+  console.log(usersList)
+
   return (
     <section>
       <h2>Users Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        <ul class="users-list">
+          {usersList.map((user, index) => {
+
+            if (user.gender === "female") {
+              backgroundColor = "bg-pink"
+            } else {
+              backgroundColor = "bg-blue"
+            }
+
+            return (
+              
+              <li className={backgroundColor} key={index}>
+                <img
+                  src={user.picture.medium}
+                  alt={`${user.name.first} ${user.name.last}`}
+                />
+                <h3>{`${user.name.title} ${user.name.first} ${user.name.last}`}</h3>
+                <p>Email: {user.email}</p>
+              </li>
+            )
+          })}
+          
+        </ul>
+      </div>
     </section>
   )
 }
